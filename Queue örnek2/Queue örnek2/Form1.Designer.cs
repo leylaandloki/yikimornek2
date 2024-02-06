628c8fa978ff265350e0cb2f46daa5209bc86b72
leylaandloki <145972621+leylaandloki@users.noreply.github.com>
2024-02-06 11:52:29 +0300

    Add files via upload

---
+++ Queue örnek2/Queue örnek2/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Queue_örnek2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSiraAl = new System.Windows.Forms.Button();
+            this.btnGise1 = new System.Windows.Forms.Button();
+            this.btnGise2 = new System.Windows.Forms.Button();
+            this.btnGise3 = new System.Windows.Forms.Button();
+            this.lblSira1 = new System.Windows.Forms.Label();
+            this.lblSira2 = new System.Windows.Forms.Label();
+            this.lblSira3 = new System.Windows.Forms.Label();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSiraAl
+            // 
+            this.btnSiraAl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSiraAl.Location = new System.Drawing.Point(37, 290);
+            this.btnSiraAl.Name = "btnSiraAl";
+            this.btnSiraAl.Size = new System.Drawing.Size(376, 79);
+            this.btnSiraAl.TabIndex = 0;
+            this.btnSiraAl.Text = "Sıra Al";
+            this.btnSiraAl.UseVisualStyleBackColor = true;
+            this.btnSiraAl.Click += new System.EventHandler(this.btnSiraAl_Click);
+            // 
+            // btnGise1
+            // 
+            this.btnGise1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGise1.Location = new System.Drawing.Point(37, 25);
+            this.btnGise1.Name = "btnGise1";
+            this.btnGise1.Size = new System.Drawing.Size(102, 70);
+            this.btnGise1.TabIndex = 1;
+            this.btnGise1.Text = "Gişe 1";
+            this.btnGise1.UseVisualStyleBackColor = true;
+            this.btnGise1.Click += new System.EventHandler(this.btnGise1_Click);
+            // 
+            // btnGise2
+            // 
+            this.btnGise2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGise2.Location = new System.Drawing.Point(173, 25);
+            this.btnGise2.Name = "btnGise2";
+            this.btnGise2.Size = new System.Drawing.Size(102, 70);
+            this.btnGise2.TabIndex = 2;
+            this.btnGise2.Text = "Gişe 2";
+            this.btnGise2.UseVisualStyleBackColor = true;
+            this.btnGise2.Click += new System.EventHandler(this.btnGise2_Click);
+            // 
+            // btnGise3
+            // 
+            this.btnGise3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnGise3.Location = new System.Drawing.Point(301, 25);
+            this.btnGise3.Name = "btnGise3";
+            this.btnGise3.Size = new System.Drawing.Size(112, 70);
+            this.btnGise3.TabIndex = 3;
+            this.btnGise3.Text = "Gişe 3";
+            this.btnGise3.UseVisualStyleBackColor = true;
+            this.btnGise3.Click += new System.EventHandler(this.btnGise3_Click);
+            // 
+            // lblSira1
+            // 
+            this.lblSira1.AutoSize = true;
+            this.lblSira1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSira1.Location = new System.Drawing.Point(58, 136);
+            this.lblSira1.Name = "lblSira1";
+            this.lblSira1.Size = new System.Drawing.Size(57, 20);
+            this.lblSira1.TabIndex = 4;
+            this.lblSira1.Text = "label1";
+            // 
+            // lblSira2
+            // 
+            this.lblSira2.AutoSize = true;
+            this.lblSira2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSira2.Location = new System.Drawing.Point(188, 136);
+            this.lblSira2.Name = "lblSira2";
+            this.lblSira2.Size = new System.Drawing.Size(57, 20);
+            this.lblSira2.TabIndex = 5;
+            this.lblSira2.Text = "label2";
+            // 
+            // lblSira3
+            // 
+            this.lblSira3.AutoSize = true;
+            this.lblSira3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSira3.Location = new System.Drawing.Point(332, 136);
+            this.lblSira3.Name = "lblSira3";
+            this.lblSira3.Size = new System.Drawing.Size(57, 20);
+            this.lblSira3.TabIndex = 6;
+            this.lblSira3.Text = "label3";
+            // 
+            // txtAd
+            // 
+            this.txtAd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtAd.Location = new System.Drawing.Point(37, 232);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(376, 26);
+            this.txtAd.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 410);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.lblSira3);
+            this.Controls.Add(this.lblSira2);
+            this.Controls.Add(this.lblSira1);
+            this.Controls.Add(this.btnGise3);
+            this.Controls.Add(this.btnGise2);
+            this.Controls.Add(this.btnGise1);
+            this.Controls.Add(this.btnSiraAl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSiraAl;
+        private System.Windows.Forms.Button btnGise1;
+        private System.Windows.Forms.Button btnGise2;
+        private System.Windows.Forms.Button btnGise3;
+        private System.Windows.Forms.Label lblSira1;
+        private System.Windows.Forms.Label lblSira2;
+        private System.Windows.Forms.Label lblSira3;
+        private System.Windows.Forms.TextBox txtAd;
+    }
+}
+
---
+++ Queue örnek2/Queue örnek2/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Queue_örnek2
+{
+    public partial class Form1 : Form
+    {
+        Queue<string>musteri=new Queue<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void musteriCagir(Label lbl)
+        {
+            try
+            {
+                lbl.Text = musteri.Dequeue();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            musteri.Enqueue("İrem");
+            musteri.Enqueue("Leyla");
+            musteri.Enqueue("Yasemin");
+        }
+
+        private void btnSiraAl_Click(object sender, EventArgs e)
+        {
+            musteri.Enqueue(txtAd.Text);
+        }
+
+        private void btnGise1_Click(object sender, EventArgs e)
+        {
+            musteriCagir(lblSira1);
+        }
+
+        private void btnGise2_Click(object sender, EventArgs e)
+        {
+            musteriCagir(lblSira2);
+        }
+
+        private void btnGise3_Click(object sender, EventArgs e)
+        {
+            musteriCagir(lblSira3);
+        }
+    }
+}
